3f85db416d554d5d2b1e06b322814964bd4912df
That_Guy <eghove@gmail.com>
2018-10-15 17:17:17 -0500

    starting to pseudocode some

---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+
+//Going to need some sort of object to store the following: 1. Question Text 2. Potential answer text. 3. Correct answer. 4. Something that happens when after a guess is made (a gif or a video or something)
+
+//Going to need variables to track correct answers and incorrect answers
+
+//some variables to hold timers
+
+//GLOBAL VARIABLES
+
+//trivia questions object
+var triviaQuestions = {
+    'Question #1' : {
+        text: 'text for the question',
+        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+        correctAnswer: 'text of correct answer',
+        image: 'image relating to the correct guess'
+    },
+
+    'Question #2' : {
+        text: 'text for the question',
+        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+        correctAnswer: 'text of correct answer',
+        image: 'image relating to the correct guess'
+    },
+
+    'Question #3' : {
+        text: 'text for the question',
+        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+        correctAnswer: 'text of correct answer',
+        image: 'image relating to the correct guess'
+    },
+
+    'Question #4' : {
+        text: 'text for the question',
+        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+        correctAnswer: 'text of correct answer',
+        image: 'image relating to the correct guess'
+    },
+
+    'Question #5' : {
+        text: 'text for the question',
+        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+        correctAnswer: 'text of correct answer',
+        image: 'image relating to the correct guess'
+    },
+
+    'Question #6' : {
+        text: 'text for the question',
+        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+        correctAnswer: 'text of correct answer',
+        image: 'image relating to the correct guess'
+    },
+
+    'Question #7' : {
+        text: 'text for the question',
+        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+        correctAnswer: 'text of correct answer',
+        image: 'image relating to the correct guess'
+    },
+
+    'Question #8' : {
+        text: 'text for the question',
+        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+        correctAnswer: 'text of correct answer',
+        image: 'image relating to the correct guess'
+    },
+
+    'Question #9' : {
+        text: 'text for the question',
+        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+        correctAnswer: 'text of correct answer',
+        image: 'image relating to the correct guess'
+    }
+
+
+}
+
+//correct answers counter
+var correctA=0;
+
+//incorrect answers counter
+var incorrectA=0;
