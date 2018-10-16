31d2761244ac60aefbf8f6b3751c0e419ab4b2da
That_Guy <eghove@gmail.com>
2018-10-16 16:27:08 -0500

    starting to build the game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,69 +8,73 @@
 //GLOBAL VARIABLES
 
 //trivia questions object
+
+$(document).ready(function() { //wrapping all of this in this onready function
 var triviaQuestions = {
-    'Question #1' : {
+    'Q1' : {
+        text: 'text for the question1',
+        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+        correctAnswer: 'text of correct answer',
+        image: 'image relating to the correct guess'
+    },
+
+    'Q2' : {
         text: 'text for the question',
         options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
         correctAnswer: 'text of correct answer',
         image: 'image relating to the correct guess'
     },
 
-    'Question #2' : {
+    'Q3' : {
         text: 'text for the question',
         options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
         correctAnswer: 'text of correct answer',
         image: 'image relating to the correct guess'
     },
 
-    'Question #3' : {
+    'Q4' : {
         text: 'text for the question',
         options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
         correctAnswer: 'text of correct answer',
         image: 'image relating to the correct guess'
     },
 
-    'Question #4' : {
+    'Q5' : {
         text: 'text for the question',
         options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
         correctAnswer: 'text of correct answer',
         image: 'image relating to the correct guess'
     },
 
-    'Question #5' : {
+    'Q6' : {
         text: 'text for the question',
         options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
         correctAnswer: 'text of correct answer',
         image: 'image relating to the correct guess'
     },
 
-    'Question #6' : {
+    'Q7' : {
         text: 'text for the question',
         options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
         correctAnswer: 'text of correct answer',
         image: 'image relating to the correct guess'
     },
 
-    'Question #7' : {
+    'Q8' : {
         text: 'text for the question',
         options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
         correctAnswer: 'text of correct answer',
         image: 'image relating to the correct guess'
     },
 
-    'Question #8' : {
+    'Q9' : {
         text: 'text for the question',
         options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
         correctAnswer: 'text of correct answer',
         image: 'image relating to the correct guess'
     },
 
-    'Question #9' : {
-        text: 'text for the question',
-        options: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
-        correctAnswer: 'text of correct answer',
-        image: 'image relating to the correct guess'
-    }
+    
 
 
 }
@@ -80,3 +84,26 @@ var correctA=0;
 
 //incorrect answers counter
 var incorrectA=0;
+
+//starting to build the function that will redner the questions
+function renderQuestion(obj) {
+    $("#triviaSpace").empty(); // empties out the previous stuff
+    var triviaBlock = $("<div>"); //creating an empty div, assigning it to this variable
+    var questionText = obj.text;
+    triviaBlock.text(questionText);
+    //need to build the for loop for possible answers
+   // $( "#triviaSpace").append(triviaBlock);
+
+};
+
+renderQuestion(triviaQuestions.Q1);
+
+
+
+
+//var triviaBlock = $( '<div>');
+//triviaBlock.text("Some awesome text");
+
+//$( "#triviaSpace").append(triviaBlock);
+
+}); //end of ready wrap function
